2c7e94b10803e13a696468516e9ed654d2edbd7f
tayurus <tayurus@gmail.com>
2023-04-24 12:05:05 +0500

    initial

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { createLogger } from 'redux-logger'
+const loggerMiddleware = createLogger({
+
+});
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    if (process.env.NODE_ENV !== 'production') {
+      return getDefaultMiddleware({ serializableCheck: false, immutableCheck: false }).concat(loggerMiddleware);
+    }
+    return getDefaultMiddleware();
   },
 });
 
